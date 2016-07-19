6753c7a21d2d7b41ad6a81ef62928b140d87b561
Levyx User <levyx@Hnode3-14.pub.pic2.ibm.com>
2016-07-19 14:53:59 -0400

    initial

---
+++ BenchSort.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.lang.*;
+import java.util.Random;
+
+class BenchSort {
+  public static void main(String[] args){
+    int len = Integer.parseInt(args[0]);
+    int[] iArr = new int[len];
+    Random random = new Random(0);
+
+    for (int i = 0; i < len; i++) {
+      iArr[i] = random.nextInt();
+    }
+    System.out.println("Sorting array with len: " + new Integer(len));
+
+    long preExecTime = System.nanoTime();
+    Arrays.sort(iArr);
+    long postExecTime = System.nanoTime();
+    System.out.println("Takes: " + new Double((postExecTime - preExecTime)/1000000.0) + "ms");
+  }
+}
+
